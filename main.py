38adfd2ab2e73fd9e9e01a85da1a7a28af08234d
mars7f <mars7f@icloud.com>
2024-10-08 20:56:57 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(6 == 8)
+
+# 4st program
+print(float('123.456'))
+print(123.456 * 10)
+print(int(1234.56))
+print(1234 % 10)
